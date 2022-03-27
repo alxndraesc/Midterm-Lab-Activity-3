675125f06b28b84d00d4e3b2f60b4967773b8e1e
Alessandra Escurel <100029708+alxndraesc@users.noreply.github.com>
2022-03-27 14:40:46 +0800

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,15 @@
+package programming2;
+
+import java.util.Scanner;
+
+public class Binary {
+	
+    public static void main(String [] args){
+          Scanner input = new Scanner(System.in);
+           System.out.println("Enter Binary num:");
+          String BinaryNumber= input.nextLine();
+          
+          int DecimalNumber= Integer.parseInt(BinaryNumber,2); 
+          System.out.println("Decimal num is:"+DecimalNumber);
+    }
+    }
